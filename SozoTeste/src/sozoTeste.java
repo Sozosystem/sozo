7fa51c226b732ea7cc934799d1e1626fe2596b90
gabriel.oliveira <gabriel.oliveira@MVBRRECDSK2823.mvrec.local>
2015-02-26 10:49:19 -0300

    oie

--- SozoTeste/src/sozoTeste.java
+++ SozoTeste/src/sozoTeste.java
@@ -4,6 +4,7 @@ public class sozoTeste {
 	public static void main(String[] args) {
 		System.out.println("sozo funcionando uhu");
         System.out.println("Edit By: Caio");
+        System.out.println("Android e Eclipse configurados. vamos a luta - gabriel-joe");
 	}
 
 }
