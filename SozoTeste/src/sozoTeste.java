d42754a1a8e9d9a706be00761b6a6e379bce870c
EberZanardi <eberweydson@gmail.com>
2015-03-04 20:33:18 -0300

    Atualizando o teste

--- SozoTeste/src/sozoTeste.java
+++ SozoTeste/src/sozoTeste.java
@@ -3,7 +3,7 @@ public class sozoTeste {
 
 	public static void main(String[] args) {
 		System.out.println("quarta-feira 04/03/2015");
-        System.out.println("Edit By: Casssssssssio");
+        System.out.println("Edit By: Eber Zanardi");
         System.out.println("Android e Eclipse configurados. vamos a luta - gabriel-joe");
 	}
 
