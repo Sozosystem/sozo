b87542a6b1d27bbca7eb82973f3e20cc8eb38a60
aluno <aluno@pc-029.EXPERT.LOCAL>
2015-03-04 19:52:23 -0300

    commit teste

--- SozoTeste/src/sozoTeste.java
+++ SozoTeste/src/sozoTeste.java
@@ -2,7 +2,7 @@
 public class sozoTeste {
 
 	public static void main(String[] args) {
-		System.out.println("sozo funcionando uhu");
+		System.out.println("adas");
         System.out.println("Edit By: Casssssssssio");
         System.out.println("Android e Eclipse configurados. vamos a luta - gabriel-joe");
 	}
