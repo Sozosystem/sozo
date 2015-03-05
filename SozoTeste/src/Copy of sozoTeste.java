a91da4438bb64f3fb5f38a47c6eda4d79f764303
069572110892 <069572110892@RPEWSEIMP34.tre-pe.gov.br>
2015-03-05 13:52:38 -0300

    2 class

---
+++ SozoTeste/src/Copy of sozoTeste.java
@@ -0,0 +1,10 @@
+
+public class sozoTeste {
+
+	public static void main(String[] args) {
+		System.out.println("quarta-feira 04/03/2015");
+        System.out.println("Edit By: silva");
+        System.out.println("Android e Eclipse configurados. vamos a luta - gabriel-joe");
+	}
+
+}
