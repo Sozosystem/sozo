c59a22fbefb66f1ebe7476a2d8f8f2bc554b3546
gabriel.oliveira <gabriel.oliveira@MVBRRECDSK2823.mvrec.local>
2015-03-07 09:28:04 -0300

    alterando classe controller de viatura, adicionando metodos cruciais

--- SozoWeb/src/controller/ViaturaController.java
+++ SozoWeb/src/controller/ViaturaController.java
@@ -12,6 +12,7 @@ import model.Viatura;
 public class ViaturaController {
 	
 	private ViaturaDAO dao;
+	private Viatura viatura;
 	
 	public ViaturaController() {
 		EntityManagerHelper emh = new EntityManagerHelper();            
@@ -21,4 +22,33 @@ public class ViaturaController {
 	public List<Viatura> getListaViaturas() {
 		return dao.findAll();
 	}
+	
+	public void salvarViatura() {
+		System.out.println(viatura);
+		Viatura v = new Viatura();
+		v.setTipo(viatura.getTipo());
+		v.setPlaca(viatura.getPlaca());
+		v.setDescricao(viatura.getDescricao());
+		dao.save(v);
+	}
+	
+	public String alterarViatura() {
+		dao.update(viatura);
+		return "index.xhtml";
+	}
+	
+	public String removerViatura() {
+		dao.delete(viatura);
+		//viatura = new viatura();
+		return "index.xhtml";
+	}
+	
+	public Viatura getViatura() {
+		return viatura;
+	}
+
+	public void setViatura(Viatura viatura) {
+		this.viatura = viatura;
+	}
+	
 }
