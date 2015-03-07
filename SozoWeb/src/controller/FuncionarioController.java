b661d22e282170221d27ef89b28ac4947ff5d378
gabriel.oliveira <gabriel.oliveira@MVBRRECDSK2823.mvrec.local>
2015-03-07 09:23:00 -0300

    retirando imports desnecessários

--- SozoWeb/src/controller/FuncionarioController.java
+++ SozoWeb/src/controller/FuncionarioController.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
