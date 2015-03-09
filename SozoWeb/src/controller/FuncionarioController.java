d1e1a3d058fb04db2ef0aabc6f8a79b7b1f2f1ff
gabriel.oliveira <gabriel.oliveira@MVBRRECDSK2823.mvrec.local>
2015-03-09 14:28:37 -0300

    Validacao de usuario

--- SozoWeb/src/controller/FuncionarioController.java
+++ SozoWeb/src/controller/FuncionarioController.java
@@ -1,25 +1,19 @@
 package controller;
 
-
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
-import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
 
-import util.Mensagem;
 import model.Funcionario;
 import model.Situacao;
 import model.TipoFuncionario;
+import util.Mensagem;
 import dao.EntityManagerHelper;
 import dao.FuncionarioDAO;
 import dao.TipoFuncionarioDAO;
 
-@ManagedBean(name="funcionario")
+@ManagedBean(name = "funcionario")
 @RequestScoped
 public class FuncionarioController {
 	private Funcionario funcionario;
@@ -30,27 +24,26 @@ public class FuncionarioController {
 	private Situacao[] situacoes = Situacao.values();
 	private List<TipoFuncionario> tipoFuncionario;
 	private TipoFuncionarioDAO tipoFuncionarioDao;
-	
+
 	public FuncionarioController() {
-		EntityManagerHelper emh = new EntityManagerHelper();            
-        dao = new FuncionarioDAO(emh.getEntityManager());
-        tipoFuncionarioDao = new TipoFuncionarioDAO(emh.getEntityManager());
-        
-        funcionario = new Funcionario();
-        funcionarioConsulta = new Funcionario();
-        listarTiposFunc();
-        mostrarTodosFuncionarios();
+		EntityManagerHelper emh = new EntityManagerHelper();
+		dao = new FuncionarioDAO(emh.getEntityManager());
+		tipoFuncionarioDao = new TipoFuncionarioDAO(emh.getEntityManager());
+
+		funcionario = new Funcionario();
+		funcionarioConsulta = new Funcionario();
+		listarTiposFunc();
+		mostrarTodosFuncionarios();
 	}
 
-	
 	public List<Funcionario> getListaFuncionarios() {
 		return listaFuncionarios;
 	}
-	
-	public void listarTiposFunc(){
+
+	public void listarTiposFunc() {
 		tipoFuncionario = tipoFuncionarioDao.findAll();
 	}
-	
+
 	public void salvarFuncionario() {
 		Funcionario f = new Funcionario();
 		f.setNome(funcionario.getNome());
@@ -58,84 +51,83 @@ public class FuncionarioController {
 		f.setSenha(funcionario.getSenha());
 		f.setTipoFuncionario(funcionario.getTipoFuncionario());
 		f.setSituacao(funcionario.getSituacao());
-		dao.save(f);
-		mostrarTodosFuncionarios();
-		funcionario = new Funcionario();
+		if (dao.verificaUsuarioFunc(funcionario.getUsuario())) {
+			dao.save(f);
+			mostrarTodosFuncionarios();
+			funcionario = new Funcionario();
+		} else {
+			Mensagem.alerta(Mensagem.ERRO, "Atenção : Usuário já existente", "");
+		}
+
 	}
-	
+
 	public void limparCampos() {
 		funcionario = new Funcionario();
 	}
-	
+
 	public void alterarFuncionario() {
 		dao.update(funcionario);
 		mostrarTodosFuncionarios();
 		funcionario = new Funcionario();
 	}
-	
+
 	public void removerFuncionario() {
 		dao.delete(funcionarioSelecionado);
 		mostrarTodosFuncionarios();
 	}
-	
+
 	public void consultarFuncionario() {
 		listaFuncionarios = dao.findByObject(funcionarioConsulta);
 	}
-	
+
 	public void funcionarioAlterarSelecionado() {
-		if(funcionarioSelecionado == null) return;
+		if (funcionarioSelecionado == null)
+			return;
 		funcionario = funcionarioSelecionado;
 	}
-	
+
 	public void mostrarTodosFuncionarios() {
 		listaFuncionarios = dao.findAll();
 	}
-	
+
 	public Funcionario getFuncionario() {
 		return funcionario;
 	}
-	
+
 	public void setFuncionario(Funcionario funcionario) {
 		this.funcionario = funcionario;
 	}
-	
-	public Funcionario getFuncionarioSelecionado() {
-        return funcionarioSelecionado;
-    }
- 
-    public void setFuncionarioSelecionado(Funcionario f) {
-        this.funcionarioSelecionado = f;
-    }
 
+	public Funcionario getFuncionarioSelecionado() {
+		return funcionarioSelecionado;
+	}
+
+	public void setFuncionarioSelecionado(Funcionario f) {
+		this.funcionarioSelecionado = f;
+	}
 
 	public Funcionario getFuncionarioConsulta() {
 		return funcionarioConsulta;
 	}
 
-
 	public void setFuncionarioConsulta(Funcionario funcionarioConsulta) {
 		this.funcionarioConsulta = funcionarioConsulta;
 	}
-
 
 	public Situacao[] getSituacoes() {
 		return situacoes;
 	}
 
-
 	public void setSituacoes(Situacao[] situacoes) {
 		this.situacoes = situacoes;
 	}
-
 
 	public List<TipoFuncionario> getTipoFuncionario() {
 		return tipoFuncionario;
 	}
 
-
 	public void setTipoFuncionario(List<TipoFuncionario> tipoFuncionario) {
 		this.tipoFuncionario = tipoFuncionario;
 	}
-    
- 
+
 }
--- SozoWeb/src/dao/FuncionarioDAO.java
+++ SozoWeb/src/dao/FuncionarioDAO.java
@@ -10,19 +10,35 @@ public class FuncionarioDAO extends GenericDAO<Integer, Funcionario> {
 	public FuncionarioDAO(EntityManager entityManager) {
 		super(entityManager);
 	}
-	
+
 	public Funcionario verificarLogin(Funcionario funcionario) throws Exception {
 
 		Funcionario funcionarioLogado = new Funcionario();
 		try {
 			funcionarioLogado = (Funcionario) this.entityManager.createQuery(
-					"from Funcionario WHERE usuario='" + funcionario.getUsuario().toString()
-							+ "' AND senha='" + funcionario.getSenha().toString() + "'").getSingleResult();
+					"from Funcionario WHERE usuario='"
+							+ funcionario.getUsuario().toString()
+							+ "' AND senha='"
+							+ funcionario.getSenha().toString() + "'")
+					.getSingleResult();
 			System.out.println(funcionarioLogado.getNome());
 		} catch (NoResultException nre) {
 			throw new Exception("Login ou senha inválidos.");
 		}
 		return funcionarioLogado;
+
+	}
+
+	public boolean verificaUsuarioFunc(String usuario) {
+		boolean resposta = true;
+		Funcionario f = new Funcionario();
+		f = (Funcionario) this.entityManager.createQuery(
+				"from Funcionario WHERE usuario='" + usuario + "'")
+				.getSingleResult();
+		if(f.getUsuario() != null){
+			resposta = false;
+		}
+		return resposta;
 
 	}
 
