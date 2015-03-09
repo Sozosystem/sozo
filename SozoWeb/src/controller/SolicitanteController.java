2c7edbd3eef60c86068a3618712d46d11d20c957
CAIO <caiodaschagas@gmail.com>
2015-03-09 18:22:37 -0300

    Melhoria na View Solicitantes

--- SozoWeb/src/controller/SolicitanteController.java
+++ SozoWeb/src/controller/SolicitanteController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 
+import util.Mensagem;
+import model.Situacao;
 import model.Solicitante;
 import dao.EntityManagerHelper;
 import dao.SolicitanteDAO;
@@ -13,19 +15,43 @@ public class SolicitanteController {
 	
 	private SolicitanteDAO dao;
 	private Solicitante solicitante;
+	private Solicitante solicitanteSelecionado;
+	private Solicitante solicitanteConsulta;
+	private List<Solicitante> listaSolicitantes;
+	private Situacao[] situacoes = Situacao.values();
+	
 	
 	public SolicitanteController(){
 		EntityManagerHelper emh = new EntityManagerHelper();            
 		dao = new SolicitanteDAO(emh.getEntityManager());
 		solicitante = new Solicitante();
+		solicitanteConsulta = new Solicitante();
+		mostrarTodosSolicitantes();
 	}
 	
-	public SolicitanteDAO getDao() {
-		return dao;
+	public void consultarSolicitante() {
+		listaSolicitantes = dao.findByObject(solicitanteConsulta);
 	}
-
-	public void setDao(SolicitanteDAO dao) {
-		this.dao = dao;
+	
+	public void alterarSolicitante() {
+		if(solicitante == null) {
+			Mensagem.alerta(Mensagem.INFO, "Selecione um solicitante", null);
+			return;
+		}
+		
+		dao.update(solicitante);
+		mostrarTodosSolicitantes();
+		solicitante = new Solicitante();
+	}
+	
+	public void mostrarTodosSolicitantes() {
+		this.listaSolicitantes = dao.findAll();
+	}
+	
+	public void alterarSolicitanteSelecionado() {
+		if (solicitanteSelecionado == null)
+			return;
+		solicitante = solicitanteSelecionado;
 	}
 
 	public Solicitante getSolicitante() {
@@ -36,9 +62,37 @@ public class SolicitanteController {
 		this.solicitante = solicitante;
 	}
 
-	public List<Solicitante> getLista() {
-			
-		return dao.findAll();
+	public Solicitante getSolicitanteSelecionado() {
+		return solicitanteSelecionado;
 	}
+
+	public Solicitante getSolicitanteConsulta() {
+		return solicitanteConsulta;
+	}
+
+	public List<Solicitante> getListaSolicitantes() {
+		return listaSolicitantes;
+	}
+
+	public void setSolicitanteSelecionado(Solicitante solicitanteSelecionado) {
+		this.solicitanteSelecionado = solicitanteSelecionado;
+	}
+
+	public void setSolicitanteConsulta(Solicitante solicitanteConsulta) {
+		this.solicitanteConsulta = solicitanteConsulta;
+	}
+
+	public void setListaSolicitantes(List<Solicitante> listaSolicitantes) {
+		this.listaSolicitantes = listaSolicitantes;
+	}
+
+	public Situacao[] getSituacoes() {
+		return situacoes;
+	}
+
+	public void setSituacoes(Situacao[] situacoes) {
+		this.situacoes = situacoes;
+	}
+
 
 }
